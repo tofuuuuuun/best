7a83927e1fe68f8ac7d9c02cf2aff8c40db3e3b4
hirano <hirakenlp21@gmail.com>
2024-08-04 09:33:16 +0900

    クラス追加

--- src/index.php
+++ src/index.php
@@ -5,23 +5,23 @@
 // }
 $title = "BEST";
 // include("./database/connect.php");
-
-
 include("./common/header.php");
 ?>
 <main>
-
-    <div class="l-albumList">
-        <div>
-            <div class="l-albumArt addButton disp-block">
-                <i class="fa-solid fa-plus txt-green"></i>
+    <div class="l-contentWrapper">
+        <div class="l-albumList">
+            <ul class="albumArtList"></ul>
+            <div class="albumAddButton">
+                <div class="l-albumArt albumArtList addButton disp-block">
+                    <i class="fa-solid fa-plus txt-green"></i>
+                </div>
             </div>
-            <ul class="albumArtList">
-            </ul>
         </div>
     </div>
 </main>
 <?php include("./parts/modal.php"); ?>
+<script type='text/javascript' defer src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<script type='text/javascript' defer src="../js/function.js"></script>
 </body>
 
 </html>
