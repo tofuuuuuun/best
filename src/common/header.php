5dd7d7cfd04c03ff0bed977f549a0c32323ed0d0
hirano <hirakenlp21@gmail.com>
2024-08-31 18:01:47 +0900

    使用していないフォルダ、ファイル内記述を削除

--- src/common/header.php
+++ src/common/header.php
@@ -10,8 +10,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/wave.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title><?php echo $title ?></title>
 </head>
 <meta name="viewport" content="width=device-width,initial-scale=1">
