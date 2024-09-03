7b46b687a3e3b5e8366d72d90f6b38ae1fda70f5
hirano <hirakenlp21@gmail.com>
2024-09-03 22:03:14 +0900

    本番環境用に調整

--- src/common/header.php
+++ src/common/header.php
@@ -6,14 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex">
     <script src="https://kit.fontawesome.com/3a6ca16033.js" crossorigin="anonymous"></script>
-    <link rel=”icon” href="../images/favicon.ico">
+
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
     <title><?php echo $title ?></title>
 </head>
-<meta name="viewport" content="width=device-width,initial-scale=1">
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-D2DJ1RF3J1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-D2DJ1RF3J1');
+</script>
 
 <body class="texture">
     <header>
--- src/index.php
+++ src/index.php
@@ -6,16 +6,16 @@ include("./common/header.php");
     <div class="contentWrapper">
         <div class="l-contentWrapper">
             <div class="startText m-bottom-3em ta-center disp-block fadeIn">
-                <h2 class="txt-white">あなたの音楽、あなたのベストアルバム</h2>
-                <p class="txt-white m-bottom-1em">今まで聴いてきた音楽の中から、<br>あなたのベスト10枚を選んでみませんか？</p>
+                <h2 class="txt-white m-bottom-05em">あなたの音楽、あなたのベストアルバム</h2>
+                <p class="txt-white">今まで聴いてきた音楽の中から、<br>あなたのベスト10枚を選んでみませんか？</p>
                 <p class="txt-white m-bottom-2em">お気に入りのアルバムを選ぶだけで、<br>あなたの音楽遍歴が一目でわかる一覧が完成します。</p>
 
-                <h3 class="txt-white">あなたの音楽の歴史を振り返る</h3>
-                <p class="txt-white m-bottom-1em">シンプルに、そして直感的に、あなたの好きなアルバムを選択。</p>
+                <h3 class="txt-white m-bottom-05em">あなたの音楽の歴史を振り返る</h3>
+                <p class="txt-white">シンプルに、そして直感的に、あなたの好きなアルバムを選択。</p>
                 <p class="txt-white m-bottom-2em">アルバムアートを一覧で表示して<br>「このアルバム、懐かしい！」なんて話題も広がるはず。</p>
 
-                <h3 class="txt-white">みんなにシェアしよう</h3>
-                <p class="txt-white m-bottom-1em">作ったリストは、みんなにシェアして<br>「このアルバム超オススメ！」って自慢しよう。</p>
+                <h3 class="txt-white m-bottom-05em">みんなにシェアしよう</h3>
+                <p class="txt-white">作ったリストは、みんなにシェアして<br>「このアルバム超オススメ！」って自慢しよう。</p>
                 <p class="txt-white m-bottom-2em">音楽の話題で盛り上がれること間違いなし！</p>
 
                 <button class="startButton bg-turquoise txt-white font-wb">さあ、始めよう！</button>
@@ -35,7 +35,7 @@ include("./common/header.php");
 <?php include("./parts/modal.php"); ?>
 <script type='text/javascript' defer src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js" integrity="sha512-BNaRQnYJYiPSqHHDb58B0yaPfCu+Wgds8Gp/gU33kqBtgNS4tSPHuGibyoeqMV/TJlSKda6FXzoEyYGjTe+vXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script type='text/javascript' src="../js/function.js" defer></script>
+<script type='text/javascript' src="./js/function.js" defer></script>
 </body>
 
 </html>
