095afadfbacb1c580c6a81832da0cb4e81e6dbd2
hirano <hirakenlp21@gmail.com>
2024-09-04 21:07:10 +0900

    metaタグを追記しました

--- src/common/header.php
+++ src/common/header.php
@@ -5,8 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex">
+    <meta property="og:title" content="BEST" />
+    <meta property="og:description" content="あなただけのベストアルバムリストを作れます" />
+    <meta property="og:image" content="../images/ogpimg.png" />
+    <meta property="og:url" content="https://rahi-lab.com" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="BEST" />
+    <meta property="og:locale" content="ja_JP" />
     <script src="https://kit.fontawesome.com/3a6ca16033.js" crossorigin="anonymous"></script>
-
     <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -23,7 +29,6 @@
         dataLayer.push(arguments);
     }
     gtag('js', new Date());
-
     gtag('config', 'G-D2DJ1RF3J1');
 </script>
 
