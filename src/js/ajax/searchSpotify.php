6a179308bb847c8fd2c7e96515348b3835b0e3e5
hirano <hirakenlp21@gmail.com>
2024-09-05 22:20:24 +0900

    上限50件以上のデータを連続して取得できるように、検索ロジックを修正

--- src/js/ajax/searchSpotify.php
+++ src/js/ajax/searchSpotify.php
@@ -14,10 +14,38 @@ if (empty($artistId)) {
     $artistId = $result->{'artists'}->{'items'}[0]->{'id'};
 }
 
-if ($type == "single") {
-    $result = $api->getArtistAlbums($artistId, ['album_type' => 'single', 'limit' => 50, 'country' => 'JP']);
+if ($type) {
+    $result = $api->getArtistAlbums($artistId, ['include_groups' => $type, 'limit' => 50, 'country' => 'JP']);
 } else {
-    $result = $api->getArtistAlbums($artistId, ['album_type' => 'album', 'limit' => 50, 'country' => 'JP']);
+    $result = $api->getArtistAlbums($artistId, ['limit' => 50, 'country' => 'JP']);
+}
+
+// $type  = 'album';
+// $result = $api->getArtistAlbums("704gz1q9ieRxZfTkhPlZGG", ['include_groups' => $type, 'limit' => 50, 'country' => 'JP']);
+// $result->items[] = $result->items;
+
+if ($result->next) {
+    $tmpResult;
+    $resultFlg = true;
+    for ($i = 1; $resultFlg == true;) {
+        // offset=50*n、limit=50で再度APIを叩く
+        $offset = 50;
+        if ($type) {
+            $tmpResult = $api->getArtistAlbums($artistId, ['include_groups' => $type, 'offset' => $offset * $i, 'limit' => 50, 'country' => 'JP']);
+        } else {
+            $tmpResult = $api->getArtistAlbums($artistId, ['limit' => 50, 'country' => 'JP']);
+        }
+        // 変えてきた中身から必要な部分を削り出してresultに連結
+        foreach ($tmpResult->items as $key => $value) {
+            $result->items[] = $tmpResult->items[$key];
+        }
+
+        if (!$tmpResult->next) {
+            $resultFlg = false;
+        }
+
+        $i++;
+    }
 }
 
 echo json_encode($result);
